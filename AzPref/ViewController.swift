7127feedc980271a508881aaf8bf3bcf9159f192
yuriy <kukunovyura29@gmail.com>
2024-02-22 11:28:46 +0400

    Initial Commit

---
+++ AzPref/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AzPref
+//
+//  Created by RMAD201 on 2/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
